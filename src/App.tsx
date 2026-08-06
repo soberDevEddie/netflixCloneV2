c294fc714ea324351a2a6bc9e2cbd655932eae9a
EddieO23 <eocampo52@gmail.com>
2026-08-06 14:40:45 -0500

    finished Navbar && mobile Navbar. Next lecture Navbar pt 7, number 81.

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // My files
 import Home from './pages/Home';
 import Watch from './pages/Watch';
+import MyList from './pages/MyList';
+import Search from './pages/Search';
+import NotFound from './pages/NotFound';
 import Navbar from './components/Navbar';
 
 const App: FC = () => {
   return (
     <Router>
       <Navbar />
-      {/* <Home/> */}
-      <Watch />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/watch' element={<Watch />} />
+        <Route path='/myList' element={<MyList />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </Router>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
-import { type FC } from 'react';
+import { useState, type FC } from 'react';
 import { Link } from 'react-router-dom';
 
 // My files
 import logo from '../assets/Netflix-LOGO.png';
+import profilePic from '../assets/profile.jpg';
+import { Bell, ChevronRight, Menu, Search, X } from 'lucide-react';
 
 const Navbar: FC = () => {
+  const [isSearchActive, setIsSearchActive] = useState<boolean>(false);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
   return (
     <header className='fixed top-0 left-0 right-0 z-50 flex flex-col px-5 md:px-10 transition-all duration-300 ease-in-out text-white'>
       <div className='flex items-center justify-between py-4'>
@@ -13,10 +18,107 @@ const Navbar: FC = () => {
           <Link to='/'>
             <img src={logo} alt='Netflix Logo' className='w-28' />
           </Link>
+
+          {/* Navigation */}
+
+          <nav className='hidden text-sm lg:flex space-x-4 '>
+            <Link to='/' className='hover:text-gray-300'>
+              Home
+            </Link>
+            <Link to='/' className='hover:text-gray-300'>
+              TV Shows
+            </Link>
+            <Link to='/' className='hover:text-gray-300'>
+              Movies
+            </Link>
+            <Link to='/' className='hover:text-gray-300'>
+              New & Popular
+            </Link>
+            <Link to='/myList' className='hover:text-gray-300'>
+              My List
+            </Link>
+            <Link to='/' className='hover:text-gray-300'>
+              Browse By Language
+            </Link>
+          </nav>
         </div>
 
-        {/* Navigation */}
-        <nav className='text-sm lg:flex space-x-4 '>
+        {/* Icons and Profile */}
+
+        <div className='flex items-center space-x-4'>
+          {/* Search container */}
+          <div
+            className={`flex relative items-center transition-all duration-300 ${isSearchActive ? 'w-72 p-2 ' : 'w-auto'}`}
+          >
+            <button
+              className={`flex items-center justify-center p-2 ${isSearchActive ? 'absolute left-0' : ''}`}
+              aria-label='Toggle Search Button'
+              onClick={() => {}}
+            >
+              {!isSearchActive && <Search size={20} color='white' />}
+            </button>
+
+            <input
+              type='text'
+              placeholder='Search'
+              aria-label='Search'
+              className={`absolute left-10 bg-black bg-opacity-75 text-white rounded-md border border-transparent focus:outline-none transition-all duration-300 ${isSearchActive ? 'w-60 p-2 border-white opacity-100' : 'w-0 p-0 opacity-0'}`}
+              onChange={() => {}}
+              onKeyDown={() => {}}
+            />
+          </div>
+
+          <Bell size={20} color='white' className='cursor-pointer' />
+
+          <img
+            src={profilePic}
+            alt='Profile Image'
+            className='w-8 h-8 rounded cursor-pointer'
+          />
+          <ChevronRight size={20} color='white' />
+          <button
+            onClick={() => {
+              setIsMenuOpen(!isMenuOpen);
+            }}
+            className='lg:hidden ml-4 focus-outline-none'
+            aria-label='Hamburger Menu'
+          >
+            <Menu size={20} color='white' />
+          </button>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+
+      <div
+        className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-95 p-8 flex flex-col gap-4 transition-transform duration-300 z-40 ${isMenuOpen ? 'translate-y-0' : 'translate-y-full'} lg:hidden`}
+      >
+        <button className={`self-end`}>
+          <X size={24} color='white' />
+        </button>
+
+        <nav className='flex flex-col space-y-2 mt-4'>
+          <div
+            className={`relative flex transition-all duration-300 ${isSearchActive ? '' : 'w-auto'}`}
+          >
+            <button
+              className={`flex items-center justify-center p-2 ${isSearchActive ? 'absolute left-0' : ''}`}
+              aria-label='Toggle Search Button'
+              onClick={() => {}}
+            >
+              {!isSearchActive && <Search size={20} color='white' />}
+            </button>
+
+            <input
+              type='text'
+              placeholder='Search'
+              aria-label='Search'
+              className={`absolute left-10 bg-black bg-opacity-75 text-white rounded-md border border-transparent focus:outline-none transition-all duration-300 ${isSearchActive ? 'w-60 p-2 border-white opacity-100' : 'w-0 p-0 opacity-0'}`}
+              onChange={() => {}}
+              onKeyDown={() => {}}
+            />
+          </div>
+
           <Link to='/' className='hover:text-gray-300'>
             Home
           </Link>
